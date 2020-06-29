9364d4f3df722d2ad5b696a6a9bd543154979d52
Hayato Clarke <hayatoclarke@gmail.com>
2020-06-30 00:57:30 +0900

    user model created and seeded. post model generated.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Destroying existing users...'
+User.destroy_all
+puts 'All users destroyed.'
+
+puts 'Creating 20 new users...'
+20.times do
+  user = User.new(
+    username: Faker::Internet.username,
+    password: Faker::Internet.password(min_length: 8),
+    email: Faker::Internet.free_email
+  )
+  user.save
+end
+puts '20 new users created'
+puts 'Seeding completed.'
